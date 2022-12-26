a6370444a307847bca436355a1927f8c0e81fe0d
Manjeet Dhayal <76167344+manjeetdhayal@users.noreply.github.com>
2022-12-26 22:35:35 +0530

    Added: html, js , css files

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+let trickyBtn = document.getElementById("move-btn"); 
+let yesBtn = document.getElementById("yes-btn"); 
+trickyBtn.addEventListener('mouseover', (e)=> {
+    trickyBtn.style.left = (Math.random() * 90) + "%"; 
+    trickyBtn.style.top = (Math.random() * 90) + "%"; 
+})
+
+trickyBtn.addEventListener('touchstart', (e) => {
+    trickyBtn.style.left = (Math.random() * 90) + "%"; 
+    trickyBtn.style.top = (Math.random() * 90) + "%"; 
+})
+
+
+yesBtn.addEventListener('click', function(){
+    Swal.fire({
+        title: 'Hehe I already Knew :3',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        confirmButtonText: 'hehe :)',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      })
+})
